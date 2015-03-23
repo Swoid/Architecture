ffd2479e77aa5e7e0a5373a52fe7b260f71a6f59
Jeremy Smith <contact@swith.fr>
2015-03-23 20:51:10 +0100

    Start Request structure

--- Core/Request.php
+++ Core/Request.php
@@ -6,13 +6,15 @@ namespace Core;
 
 class Request 
 {
+	public $url = null;
 
 	public function __construct()
 	{
 		if (isset($_SERVER['PATH_INFO'])) {
-			
+			$this->url = $_SERVER['PATH_INFO'];
+		} else {
+			$this->url = "d_controller/d_action";
 		}
-		
 	}
 
 } 
