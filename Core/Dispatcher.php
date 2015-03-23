481bc9410e46be6da5259339dbb6e17f8ed93844
Jeremy Smith <contact@swith.fr>
2015-03-23 20:50:51 +0100

    Start Dispatcher structure

--- Core/Dispatcher.php
+++ Core/Dispatcher.php
@@ -10,7 +10,17 @@ class Dispatcher
 	public function __construct()
 	{
 		$this->Request = new Request();
-		
+
+		Router::parse($this->Request);
+
+		$route = $this->Request->controller . '/' . $this->Request->action;
+
+		if (in_array($route, include(ROUTES))) {
+			die("yes");
+		} else {
+			die("no");
+		}
+
 	}
 
 } 
