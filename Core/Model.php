3e28726acc3aa1ba4056501dea27e7284ea67bdc
Jeremy Smith <contact@swith.fr>
2015-04-25 11:42:53 +0200

    get connexion to database

--- Core/Model.php
+++ Core/Model.php
@@ -6,5 +6,24 @@ namespace Core;
 
 class Model 
 {
+    /**
+     * La connexion PDO
+     * @var null|PDO
+     */
+    protected $db = null;
+
+    /**
+     * Le nom de la table
+     * @var null|string
+     */
+    protected $table = null;
+
+    function __construct()
+    {
+        if (is_null($this->db))
+            $this->db = DbProvider::getDb();
+
+        $this->table = str_replace('models\\', '', strtolower(get_class($this))) . 's';
+    }
 
 } 
