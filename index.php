eab913beeff1bf87f1a504cc6e7a879c8cf5d043
Jeremy Smith <contact@swith.fr>
2015-04-25 11:41:51 +0200

    add constantes

--- index.php
+++ index.php
@@ -1,6 +1,12 @@
 <?php
-# Inclure les constantes
-include('./Core/Configs/constantes.php');
+# Les constantes
+define('ROUTES', './App/Configs/routes.php');
+define('DS', DIRECTORY_SEPARATOR);
+define('BASE'   , __DIR__);
+define('ROOT'   , str_replace('index.php', '', $_SERVER['SCRIPT_NAME']));
+define('CORE'   , ROOT.'Core'.DS);
+define('APP'    , ROOT.'App'.DS);
+define('ASSETS', APP."Assets".DS);
 
 # Require autoloader
 require('./vendor/autoload.php');
