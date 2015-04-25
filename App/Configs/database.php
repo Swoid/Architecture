a9123ed28374208ded09aad6242cd35b39c0bcd0
Jeremy Smith <contact@swith.fr>
2015-04-25 11:38:46 +0200

    add database config

--- App/Configs/database.php
+++ App/Configs/database.php
@@ -1,5 +1,13 @@
-<?php 
+<?php
+
 
 return [
-
+    'host'     => 'localhost',
+    'dbName'   => 'swoid',
+    'username' => 'root',
+    'password' => 'root',
+    'options'  => [
+        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_OBJ,
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+    ]
 ];
