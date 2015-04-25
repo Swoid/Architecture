9c87552879d884cab1d319bc4d55ad90a74d2862
Jeremy Smith <contact@swith.fr>
2015-04-25 11:42:33 +0200

    update system

--- Core/Dispatcher.php
+++ Core/Dispatcher.php
@@ -15,11 +15,22 @@ class Dispatcher
 
 		$route = $this->Request->controller . '/' . $this->Request->action;
 
-		if (in_array($route, include(ROUTES))) {
-			die("yes");
-		} else {
-			die("no");
-		}
+        if (in_array($route, include(ROUTES))) {
+            $controllerName = 'App\\Controllers\\' . ucfirst($this->Request->controller) . 'Controller';
+            $controller = new $controllerName;
+
+            $data = call_user_func([$controller,$this->Request->action]);
+            if(!empty($data)) {
+                extract($data);
+            }
+
+            ob_start();
+            require('./App/Views/' . ucfirst($this->Request->controller) . '/' . $controller->view . '.php');
+            $layout_content = ob_get_clean();
+            require('./App/Views/Layouts/' . $controller->layout . '.php');
+        } else {
+            die("Page introuvable");
+        }
 
 	}
 
