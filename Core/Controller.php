f9eef99b1ea0aab4d979089fc89dc0872c85e86c
Jeremy Smith <contact@swith.fr>
2015-04-26 11:57:28 +0200

    update main controller

--- Core/Controller.php
+++ Core/Controller.php
@@ -3,10 +3,35 @@
 
 namespace Core;
 
-
-class Controller 
+class Controller
 {
     public $view = null,
-        $layout = 'default';
+           $layout = 'default',
+           $Session = false,
+           $name = null,
+           $Request = null;
 
-} 
+    function __construct(Request $request, $name)
+    {
+        $this->Request = $request;
+        $this->name = $name;
+        $this->view = $request->action;
+        if (!$this->Session) {
+            $this->Session = new Session();
+        }
+        $this->loadModel();
+    }
+
+
+    public function loadModel($name = null)
+    {
+        if(is_null($name)){
+            $name = ucfirst(substr($this->name, 0, -1));
+        }
+
+        $className = 'App\\Models\\' . $name;
+
+        $this->$name = new $className();
+    }
+}
+
