c465380e5db06aa3f59699c73d554d5c9522389f
Jeremy Smith <contact@swith.fr>
2015-04-26 14:30:39 +0200

    add validation rules

--- App/Models/User.php
+++ App/Models/User.php
@@ -4,7 +4,20 @@
 namespace App\Models;
 
 
+use Core\Validator;
+
 class User extends AppModel
 {
+    use Validator;
 
+    public $validationRules = [
+        'username' => [
+            ['ruleName'=>'required'],
+            ['ruleName'=>'isString']
+        ],
+        'password' => [
+            ['ruleName'=>'required'],
+            ['ruleName'=>'isString']
+        ]
+    ];
 } 
