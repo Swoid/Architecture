d7f01bb1b2b8c48257e7ce2de5f656733b21556e
Jeremy Smith <contact@swith.fr>
2015-04-26 14:31:32 +0200

    update controller

--- Core/Controller.php
+++ Core/Controller.php
@@ -10,7 +10,9 @@ class Controller
            $Session = false,
            $rendered = false,
            $name = null,
-           $Request = null;
+           $Request = null,
+           $vars = [],
+           $Auth = null;
 
     function __construct(Request $request, $name)
     {
@@ -19,6 +21,9 @@ class Controller
         $this->view = $request->action;
         if (!$this->Session) {
             $this->Session = new Session();
+        }
+        if (!$this->Auth) {
+            $this->Auth = new Auth();
         }
         $this->loadModel();
     }
@@ -37,13 +42,33 @@ class Controller
 
     public function render()
     {
-        if(!$this->rendered) {
-            ob_start();
-            require('./App/Views/' . ucfirst($this->name) . '/' . $this->view . '.php');
-            $layout_content = ob_get_clean();
-            require('./App/Views/Layouts/' . $this->layout . '.php');
-            $this->rendered = true;
+        if($this->rendered) {
+            return true;
         }
+
+        extract($this->vars);
+        ob_start();
+        require('./App/Views/' . ucfirst($this->name) . '/' . $this->view . '.php');
+        $layout_content = ob_get_clean();
+        require('./App/Views/Layouts/' . $this->layout . '.php');
+        $this->rendered = true;
+    }
+
+
+    /**
+     * Permet de définir les variables à envoyer à la vue
+     * @param $key   le nom de la variable
+     * @param $value sa valeur
+     * @return array le tableau des variables
+     */
+    public function set($key, $value = null)
+    {
+        if (is_array($key)) {
+            $this->vars += $key;
+        } else {
+            $this->vars[$key] = $value;
+        }
+
     }
 }
 
