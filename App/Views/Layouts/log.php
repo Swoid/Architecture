005b626d5f5b01152d896ae73d09494034d96df1
Jeremy Smith <contact@swith.fr>
2015-04-26 14:32:23 +0200

    add logo.svg

---
+++ App/Views/Layouts/log.php
@@ -0,0 +1,12 @@
+<?php use Core\Helpers\Html; ?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<meta charset="UTF-8">
+	<?= Html::css('main'); ?>
+</head>
+<body id="private">
+    <?= $layout_content; ?>
+</body>
+</html>
