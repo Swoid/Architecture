6a25168c8c3789c0c1fcdb7ff288de83110d48f4
Jeremy Smith <contact@swith.fr>
2015-04-26 14:29:58 +0200

    add login and register

--- App/Controllers/UsersController.php
+++ App/Controllers/UsersController.php
@@ -6,5 +6,35 @@ namespace App\Controllers;
 
 class UsersController extends AppController
 {
+    public function login()
+    {
+        $this->layout = 'log';
+        $d['errors'] = [];
+        if($this->Request->isPost) {
+            if($this->User->validate($this->Request->data)) {
+                if($this->Auth->login($this->User,$this->Request->data)){
+                    die('ouiiiiiiii');
+                }else{
+                    die("noooooon");
+                }
+            }else {
+                $d['errors'] = $this->User->errors();
+            }
+        }
+        $this->set($d);
+    }
 
+    public function register()
+    {
+        $this->layout = 'log';
+        $d['errors'] = [];
+        if($this->Request->isPost){
+            if($this->User->validate($this->Request->data)) {
+                $this->Auth->register($this->User,$this->Request->data);
+            }else {
+                $d['errors'] = $this->User->errors();
+            }
+        }
+        $this->set($d);
+    }
 } 
