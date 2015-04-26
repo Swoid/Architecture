fa2d1ca5dd984b1e5e5dad41ce3090d00ba52313
Jeremy Smith <contact@swith.fr>
2015-04-26 12:19:56 +0200

    add helper html

---
+++ Core/Helpers/Html.php
@@ -0,0 +1,23 @@
+<?php
+
+
+namespace Core\Helpers;
+
+
+class Html
+{
+    public static function css($name)
+    {
+        return " <link rel='stylesheet' type='text/css' href='" . ASSETS . "css/" . $name . ".css'>";
+    }
+
+    public static function img($name)
+    {
+        return '<img src="' . ASSETS . 'img/' . $name . '">';
+    }
+
+    public static function script($name)
+    {
+        return '<script type="text/javascript" src="'. ASSETS .'js/' . $name . '.js"></script>';
+    }
+}
