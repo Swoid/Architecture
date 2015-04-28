322f2aea7d4e0355096c155984f189fc1af5c48d
Jeremy Smith <contact@swith.fr>
2015-04-28 21:15:55 +0200

    add beforeFilter function

--- App/Controllers/AppController.php
+++ App/Controllers/AppController.php
@@ -4,8 +4,21 @@
 namespace App\Controllers;
 
 use Core\Controller;
+use Core\Helpers\Cookies;
 
 class AppController extends Controller
 {
-
+    public function beforeFilter()
+    {
+        $route = $this->Request->controller . '/' . $this->Request->action;
+        if( $route != 'users/connect') {
+            if(!isset($_COOKIE['username']) || !isset($_COOKIE['token'])) {
+                header('Location: ' . ROOT . 'users/connect');
+            } else {
+                if(!$this->Auth->rememberLogin(Cookies::get('username'))) {
+                    header('Location: ' . ROOT . 'users/connect');
+                }
+            }
+        }
+    }
 }
