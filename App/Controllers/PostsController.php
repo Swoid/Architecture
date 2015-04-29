30338c5bf8aaea6afc686d49da2b2983ed5aad5c
Jeremy Smith <contact@swith.fr>
2015-04-29 22:30:50 +0200

    add index function

--- App/Controllers/PostsController.php
+++ App/Controllers/PostsController.php
@@ -6,5 +6,18 @@ namespace App\Controllers;
 
 class PostsController extends AppController
 {
-
+    public function index(){
+        $d['posts'] = $this->Post->getHomePosts($_SESSION['id']);
+        $this->loadModel('Comment');
+        foreach ($d['posts'] as $post) {
+            $post->comments = $this->Comment->get(
+                [
+                    'where'=>'post_id = '.$post->p_id,
+                    'order'=>'date DESC',
+                    'joins' => ['users']
+                ]
+            );
+        }
+        $this->set($d);
+    }
 } 
