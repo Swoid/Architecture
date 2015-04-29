cdd1b21c42a58dbc1a21a70bbb418dddf8b8d0b8
Jeremy Smith <contact@swith.fr>
2015-04-29 22:29:17 +0200

    verif if isset session id

--- App/Controllers/AppController.php
+++ App/Controllers/AppController.php
@@ -13,9 +13,13 @@ class AppController extends Controller
         $route = $this->Request->controller . '/' . $this->Request->action;
 
         if($route != 'users/connect' && $route != 'users/logout' && $route != 'users/clear' && $route != 'users/register') {
-            if(isset($_COOKIE['username']) && isset($_COOKIE['token'])){
+            if(isset($_SESSION['id']) && isset($_SESSION['username'])){
+                return true;
+            }elseif(isset($_COOKIE['username']) && isset($_COOKIE['token'])){
                 if(!$this->Auth->rememberLogin(Cookies::get('username'))){
                     $this->redirect('users/connect');
+                } else {
+                    return true;
                 }
             } else {
                 $this->redirect('users/connect');
