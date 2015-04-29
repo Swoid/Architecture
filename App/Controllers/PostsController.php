4e66bb649eac8cf1fc75beb2d2194a804592f1a0
Jeremy Smith <contact@swith.fr>
2015-04-29 22:47:12 +0200

    get target

--- App/Controllers/PostsController.php
+++ App/Controllers/PostsController.php
@@ -9,7 +9,11 @@ class PostsController extends AppController
     public function index(){
         $d['posts'] = $this->Post->getHomePosts($_SESSION['id']);
         $this->loadModel('Comment');
+        $this->loadModel('User');
         foreach ($d['posts'] as $post) {
+            if($post->author_id != $post->target_id) {
+                $post->target = $this->User->getFirst(['where'=>'id = ' . $post->target_id]);
+            }
             $post->comments = $this->Comment->get(
                 [
                     'where'=>'post_id = '.$post->p_id,
