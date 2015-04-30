39830f01cfcf88cc78e70860299b8c25ab721d43
Jeremy Smith <contact@swith.fr>
2015-04-30 17:57:08 +0200

    add footer element

--- App/Views/Layouts/default.php
+++ App/Views/Layouts/default.php
@@ -83,28 +83,4 @@
         <?= $layout_content; ?>
     </div>
 </main>
-<div id="footer">
-    <div class="container">
-        <div class="nav">
-            <a href="<?= Html::href('posts/index'); ?>">Accueil</a>
-            <a href="<?= Html::href('users/logout'); ?>">Se déconnecter</a>
-            <div class="search search-secondary">
-                <button class="popup-button">Search</button>
-                <div class="popup">
-                    <form>
-                        <div>
-                            <input type="search">
-                            <input type="submit">
-                        </div>
-                    </form>
-                </div>
-            </div>
-            <div class="right">
-                <a href="<?= Html::href('users/index'); ?>"class="username"><?= $_SESSION['username']; ?></a>
-            </div>
-        </div>
-    </div>
-</div>
-<?= Html::script('popup'); ?>
-</body>
-</html>
+<?= $this->element('footer'); ?>
