8eefcabaf625ba8ffeea46d008a91179a50b559f
Jeremy Smith <contact@swith.fr>
2015-04-30 21:51:14 +0200

    syntaxe

--- App/Views/Posts/index.php
+++ App/Views/Posts/index.php
@@ -1,23 +1,25 @@
-<?php use Core\Helpers\Html; ?>
+<?php ?>
 <div id="left">
-    <?php foreach($posts as $post): ?>
-        <?php if($post->p_id%2 != 0): ?>
+    <?php foreach ($posts as $post): ?>
+        <?php if ($post->p_id % 2 != 0): ?>
             <div class="post">
                 <div class="header">
-                    <img src="<?= ASSETS .'img/' . $post->avatar; ?>" alt="" width="59"/>
-                    <span><?= $post->firstname . ' ' . $post->lastname; ?> <?= isset($post->target) ? '-> ' . $post->target->firstname . ' ' . $post->target->lastname  :''; ?></span>
+                    <img src="<?= ASSETS . 'img/' . $post->avatar; ?>" alt="" width="59"/>
+                    <span><?= $post->firstname . ' ' . $post->lastname; ?> <?= isset($post->target) ? '-> ' . $post->target->firstname . ' ' . $post->target->lastname : ''; ?></span>
+
                     <p class="date"><?= $post->date; ?></p>
                 </div>
                 <p class="main">
                     <?= $post->text; ?>
                 </p>
+
                 <div class="footer">
                     <button class="popup-button"><?= $post->comment_count; ?> commentaires</button>
                     <div class="popup">
                         <ul>
                             <li>
-                                <?php foreach($post->comments as $comment): ?>
-                                    <img src="<?= ASSETS .'img/' . $comment->avatar; ?>" alt="" width="43"/>
+                                <?php foreach ($post->comments as $comment): ?>
+                                    <img src="<?= ASSETS . 'img/' . $comment->avatar; ?>" alt="" width="43"/>
                                     <span><?= $comment->text; ?></span>
                                     <span class="time"><?= $comment->date; ?></span>
                                 <?php endforeach; ?>
@@ -34,24 +36,27 @@
     <?php endforeach; ?>
 </div>
 <div id="right">
-    <?php foreach($posts as $post): ?>
-        <?php if($post->p_id%2 == 0): ?>
+    <?php foreach ($posts as $post): ?>
+        <?php if ($post->p_id % 2 == 0): ?>
             <div class="post">
                 <div class="header">
-                    <img src="<?= ASSETS .'img/' . $post->avatar; ?>" alt="" width="59"/>
-                    <span><?= $post->firstname . ' ' . $post->lastname; ?> <?= isset($post->target) ? '-> ' . $post->target->firstname . ' ' . $post->target->lastname  :''; ?></span>
+                    <img src="<?= ASSETS . 'img/' . $post->avatar; ?>" alt="" width="59"/>
+                    <span class="first"><?= $post->firstname . ' ' . $post->lastname; ?></span>
+                    <?= isset($post->target) ? '<span>' . $post->target->firstname . ' ' . $post->target->lastname . '</span>' : ''; ?>
+
                     <p class="date"><?= $post->date; ?></p>
                 </div>
                 <p class="main">
                     <?= $post->text; ?>
                 </p>
+
                 <div class="footer">
                     <button class="popup-button"><?= $post->comment_count; ?> commentaires</button>
                     <div class="popup">
                         <ul>
                             <li>
-                                <?php foreach($post->comments as $comment): ?>
-                                    <img src="<?= ASSETS .'img/' . $comment->avatar; ?>" alt="" width="43"/>
+                                <?php foreach ($post->comments as $comment): ?>
+                                    <img src="<?= ASSETS . 'img/' . $comment->avatar; ?>" alt="" width="43"/>
                                     <span><?= $comment->text; ?></span>
                                     <span class="time"><?= $comment->date; ?></span>
                                 <?php endforeach; ?>
