1020050901c76a30b70e24202a88a3b37f8332f4
Jeremy Smith <contact@swith.fr>
2015-04-30 21:48:54 +0200

    add profile view

---
+++ App/Views/Users/index.php
@@ -0,0 +1,107 @@
+<?php use Core\Helpers\Html; ?>
+<main id="main">
+    <div class="container">
+        <div id="left">
+            <?php foreach($ownPosts as $oPost): ?>
+                <div class="post">
+                    <div class="header">
+                        <?= Html::img($user->avatar . '-m.png'); ?>
+                        <span class="first"><?= $user->firstname . ' ' . $user->lastname; ?></span>
+                        <?php if(isset($oPost->target)): ?>
+                            <span> -> <?= $oPost->target->firstname . ' ' . $oPost->target->lastname; ?></span>
+                        <?php endif; ?>
+                        <p class="date"<?= $oPost->date; ?></p>
+                    </div>
+                    <p class="main">
+                        <?= $oPost->text; ?>
+                    </p>
+                    <div class="footer">
+                        <button class="popup-button"><?= $oPost->comment_count; ?> commentaires</button>
+                        <div class="popup">
+                            <ul>
+                                <li>
+                                    <img src="./img/avatar.png">
+                                    <span>Message bidon</span>
+                                    <span class="time">16:32</span>
+                                </li>
+                                <li>
+                                    <img src="./img/avatar.png">
+                                    <span>Message bidon</span>
+                                    <span class="time">16:32</span>
+                                </li>
+                                <li>
+                                    <img src="./img/avatar.png">
+                                    <span>Message bidon</span>
+                                    <span class="time">16:32</span>
+                                </li>
+                            </ul>
+                            <form>
+                                <input type="text">
+                                <input type="submit">
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            <?php endforeach; ?>
+        </div>
+        <div id="right">
+            <div class="publish friend">
+                <?= Html::img($_SESSION['username'] . '-m.png'); ?>
+                <?php if($user->id == $_SESSION['id']): ?>
+                    <form>
+                        <div>
+                            <input type="text" placeholder="Publier un message sur votre page ..." >
+                            <input type="submit">
+                        </div>
+                    </form>
+                <?php else: ?>
+                    <form>
+                        <div>
+                            <input type="text" placeholder="Publier un message sur la page d'Adrien ..." >
+                            <input type="submit">
+                        </div>
+                    </form>
+                <?php endif; ?>
+            </div>
+            <?php foreach($recevedPosts as $rPost): ?>
+                <div class="post friend">
+                    <div class="header">
+                        <?= Html::img($rPost->avatar . '-m.png'); ?>
+                        <span class="first"><?= $rPost->firstname . ' ' . $rPost->lastname; ?></span>
+                        <span><?= $user->firstname . ' ' . $user->lastname; ?></span>
+                        <p class="date"><?= $rPost->date; ?></p>
+                    </div>
+                    <p class="main">
+                        <?= $rPost->text; ?>
+                    </p>
+                    <div class="footer">
+                        <button class="popup-button"><?= $rPost->comment_count; ?> commentaires</button>
+                        <div class="popup">
+                            <ul>
+                                <li>
+                                    <img src="./img/avatar.png">
+                                    <span>Message bidon</span>
+                                    <span class="time">16:32</span>
+                                </li>
+                                <li>
+                                    <img src="./img/avatar.png">
+                                    <span>Message bidon</span>
+                                    <span class="time">16:32</span>
+                                </li>
+                                <li>
+                                    <img src="./img/avatar.png">
+                                    <span>Message bidon</span>
+                                    <span class="time">16:32</span>
+                                </li>
+                            </ul>
+                            <form>
+                                <input type="text">
+                                <input type="submit">
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            <?php endforeach; ?>
+        </div>
+    </div>
+</main>
