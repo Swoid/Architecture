8f29db4e707dcb9696c6b4560f20b2bdaf9e09af
Jeremy Smith <contact@swith.fr>
2015-04-30 21:50:11 +0200

    add isFirend function

--- App/Models/User.php
+++ App/Models/User.php
@@ -20,4 +20,21 @@ class User extends AppModel
             ['ruleName'=>'isString']
         ]
     ];
+
+    /**
+     * Vérifie qu'un utilisateur fait parti de nos contact
+     * @param $user_id
+     * @param $friend_id
+     * @return mixed
+     */
+    public function isFriend($user_id,$friend_id)
+    {
+        $sql = 'SELECT COUNT(users.id) as count FROM users
+                LEFT JOIN  relations ON user_id = users.id
+                WHERE user_id = :user_id AND friend_id = :friend_id';
+        $pdost = $this->db->prepare($sql);
+        $pdost->execute([':user_id'=>$user_id,':friend_id'=>$friend_id]);
+
+        return $pdost->fetch()->count;
+    }
 } 
