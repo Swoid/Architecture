ce031158372576d6ea89aa1cb38eb501df5ca2eb
Jeremy Smith <contact@swith.fr>
2015-05-03 13:59:34 +0200

    add conversation view

---
+++ App/Views/Messages/conversation.php
@@ -0,0 +1,18 @@
+<?php use Core\Helpers\Html; ?>
+<?php foreach($messages as $message): ?>
+    <div class="row">
+        <div class="pm <?= $message->author_id != $_SESSION['id'] ? 'friend-pm' : ''; ?>">
+            <div class="header">
+                <?php if($message->author_id != $_SESSION['id']): ?>
+                    <?=  Html::img($message->avatar . '-m.png'); ?>
+                <?php else: ?>
+                    <?= Html::img($me->avatar . '-m.png'); ?>
+                <?php endif; ?>
+                <span class="time"><?= $message->date; ?></span>
+            </div>
+            <p class="main">
+               <?= $message->text; ?>
+            </p>
+        </div>
+    </div>
+<?php endforeach; ?>
