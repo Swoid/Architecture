e8a358837adf5a6c4e42777259e614546dae47d4
Jeremy Smith <contact@swith.fr>
2015-05-03 12:18:09 +0200

    add getFriends function

--- App/Models/User.php
+++ App/Models/User.php
@@ -12,12 +12,12 @@ class User extends AppModel
 
     public $validationRules = [
         'username' => [
-            ['ruleName'=>'required'],
-            ['ruleName'=>'isString']
+            ['ruleName' => 'required'],
+            ['ruleName' => 'isString']
         ],
         'password' => [
-            ['ruleName'=>'required'],
-            ['ruleName'=>'isString']
+            ['ruleName' => 'required'],
+            ['ruleName' => 'isString']
         ]
     ];
 
@@ -27,14 +27,29 @@ class User extends AppModel
      * @param $friend_id
      * @return mixed
      */
-    public function isFriend($user_id,$friend_id)
+    public function isFriend($user_id, $friend_id)
     {
         $sql = 'SELECT COUNT(users.id) as count FROM users
                 LEFT JOIN  relations ON user_id = users.id
                 WHERE user_id = :user_id AND friend_id = :friend_id';
         $pdost = $this->db->prepare($sql);
-        $pdost->execute([':user_id'=>$user_id,':friend_id'=>$friend_id]);
+        $pdost->execute([':user_id' => $user_id, ':friend_id' => $friend_id]);
 
         return $pdost->fetch()->count;
+    }
+
+    /**
+     * Récupère les amis d'un utilisateur
+     * @param $user_id
+     * @return array
+     */
+    public function getFriends($user_id)
+    {
+        $sql = 'SELECT avatar, firstname, lastname, users.id FROM users
+                LEFT JOIN relations ON friend_id = users.id
+                WHERE user_id = :user_id';
+        $pdost = $this->db->prepare($sql);
+        $pdost->execute([':user_id' => $user_id]);
+        return $pdost->fetchAll();
     }
 } 
