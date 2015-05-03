8d2c2c723c550c969ecec7408789fddb01f8742a
Jeremy Smith <contact@swith.fr>
2015-05-03 14:00:25 +0200

    add validationRules and joins

--- App/Models/Message.php
+++ App/Models/Message.php
@@ -4,7 +4,20 @@
 namespace App\Models;
 
 
+use Core\Validator;
+
 class Message extends AppModel
 {
+    use Validator;
+
+    public $joins = [
+        'users' => 'author_id'
+    ];
+
+    public $validationRules = [
+      'text' => [
+          ['ruleName'=>'required', 'message' => 'Veuillez entrer un message !']
+      ]
+    ];
 
 } 
