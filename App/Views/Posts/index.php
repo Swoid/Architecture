1f34caab17f163a813160f925cda5be14f25a1dd
Jeremy Smith <contact@swith.fr>
2015-05-03 15:14:22 +0200

    fix span !

--- App/Views/Posts/index.php
+++ App/Views/Posts/index.php
@@ -5,7 +5,8 @@
             <div class="post">
                 <div class="header">
                     <img src="<?= ASSETS . 'img/' . $post->avatar . '-m.png'; ?>" alt="" width="59"/>
-                    <span class="first"><?= $post->firstname . ' ' . $post->lastname; ?> <?= isset($post->target) ? ' ' . $post->target->firstname . ' ' . $post->target->lastname : ''; ?></span>
+                    <span class="first"><?= $post->firstname . ' ' . $post->lastname; ?></span>
+                    <span><?= isset($post->target) ? ' ' . $post->target->firstname . ' ' . $post->target->lastname : ''; ?></span>
 
                     <p class="date"><?= $post->date; ?></p>
                 </div>
