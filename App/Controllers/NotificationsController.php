947e82d9b2942faf9fa3fb1c6c48a5ecc3c780c2
Jeremy Smith <contact@swith.fr>
2015-05-04 13:07:31 +0200

    update conditions

--- App/Controllers/NotificationsController.php
+++ App/Controllers/NotificationsController.php
@@ -7,12 +7,17 @@ namespace App\Controllers;
 class NotificationsController extends AppController
 {
 
+    /**
+     * Récupère les notifications de message
+     */
     public function getMessages()
     {
         $this->layout = 'empty';
         $d['notifs'] = $this->Notification->get(
             [
-                'where' => 'ref = "messages" AND target_id = ' . $_SESSION['id'] . ' AND seen = 0',
+                'fields'=> 'messages.author_id, avatar, text',
+                'joins' => ['users','messages'],
+                'where' => "ref = 'messages' AND notifications.target_id = " . $_SESSION['id'] . " AND notifications.seen = 0",
                 'limit' => 5
             ]
         );
@@ -20,13 +25,16 @@ class NotificationsController extends AppController
         $this->set($d);
     }
 
+    /**
+     * Récupère le nombre de messages non lus
+     */
     public function getMessageCount()
     {
         $this->layout = 'empty';
         $d['count'] = $this->Notification->getFirst(
             [
                 'fields' => 'COUNT(id) as count',
-                'where' => 'ref = "messages" AND target_id = ' . $_SESSION['id'] . ' AND seen = 0',
+                'where' => 'ref = "messages" AND notifications.target_id = ' . $_SESSION['id'] . ' AND notifications.seen = 0',
             ]
         );
         $this->set($d);
