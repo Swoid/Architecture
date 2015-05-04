8f47ac6d652ef835608251465fb6e9fa91638866
Jeremy Smith <contact@swith.fr>
2015-05-04 13:08:00 +0200

    if empty notifs

--- App/Views/Notifications/getMessages.php
+++ App/Views/Notifications/getMessages.php
@@ -1,10 +1,16 @@
 <?php use Core\Helpers\Html; ?>
-<?php foreach($notifs as $notif): ?>
+<?php if(empty($notifs)): ?>
     <li>
-        <a href="<?= Html::href('messages/conversation/' . $notif->author_id); ?>">
-            <?= Html::img('swoid'); ?>
-            <span><?= $notif->author_id; ?></span>
-            <span class="time">16:32</span>
-        </a>
+       <span>Pas de nouvelle notifications</span>
     </li>
-<?php endforeach; ?>
+<?php else: ?>
+    <?php foreach($notifs as $notif): ?>
+        <li>
+            <a href="<?= Html::href('messages/conversation/' . $notif->author_id); ?>">
+                <?= Html::img($notif->avatar); ?>
+                <span><?= $notif->text; ?></span>
+                <span class="time">16:32</span>
+            </a>
+        </li>
+    <?php endforeach; ?>
+<?php endif; ?>
