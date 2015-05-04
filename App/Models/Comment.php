1ab25441cdf260e57348c22f678a8cc875f3f744
Jeremy Smith <contact@swith.fr>
2015-05-04 19:31:00 +0200

    add validations

--- App/Models/Comment.php
+++ App/Models/Comment.php
@@ -4,9 +4,20 @@
 namespace App\Models;
 
 
+use Core\Validator;
+
 class Comment extends AppModel
 {
+
+    use Validator;
+
     public $joins = [
         'users' => 'author_id'
+    ];
+
+    public $validationRules = [
+        'text' => [
+            ['ruleName'=>'required', 'message'=>'Un commentaire vide, c‘est pas cool...']
+        ]
     ];
 } 
