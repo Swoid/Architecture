28ed6154b38a2840104d0064bfad5d79b925b13a
Jeremy Smith <contact@swith.fr>
2015-05-04 13:08:27 +0200

    add joins

--- App/Models/Notification.php
+++ App/Models/Notification.php
@@ -6,6 +6,18 @@ namespace App\Models;
 
 class Notification extends AppModel
 {
+    public $joins = [
+        'messages' => 'ref_id',
+        'users'    => 'author_id',
+        'comments' => 'ref_id',
+        'relations'=> 'author_id'
+    ];
+
+    /**
+     * Marque un message comme étant lu
+     * @param $friend_id
+     * @param $user_id
+     */
     public function markMessagesAsRead($friend_id, $user_id)
     {
         $sql = "UPDATE notifications SET seen = 1 WHERE author_id = $friend_id  AND target_id = $user_id";
